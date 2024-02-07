960be196684779c023e578fe7e4f58c0e595fbed
Artyouhan <108428331+Artyouhan@users.noreply.github.com>
2024-02-08 02:32:38 +0300

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,37 +1,10 @@
-import tooltip from "./popover";
+const btn = document.querySelector('.popover-btn');
 
-const container = document.querySelector(".container");
-
-const data = {
-  "btn-1": {
-    title: "Popover first",
-    message: "Message-1",
-  },
-  "btn-2": {
-    title: "Popover second",
-    message: "Message-2",
-  },
-  "btn-3": {
-    title: "Popover third",
-    message: "Message-3",
-  },
-  "btn-4": {
-    title: "Popover fourth",
-    message: "Message-4",
-  },
-};
-
-container.addEventListener("click", (event) => {
-  event.preventDefault();
-  const { target } = event;
-  const btn = target.closest(".btn");
-
-  if (btn) {
-    let popover = btn.querySelector(".popover");
-    if (popover) {
-      popover.classList.toggle("hidden");
-    } else {
-      popover = tooltip(data[btn.id], btn);
-    }
-  }
+btn.addEventListener('click', () => {
+  const popoverElem = document.querySelector('.popover');
+  popoverElem.classList.toggle('hidden');
+  popoverElem.style.width = `${btn.offsetWidth + 20}px`;
+  const { top, left } = btn.getBoundingClientRect();
+  popoverElem.style.top = `${window.scrollY + top - popoverElem.offsetHeight}px`;
+  popoverElem.style.left = `${window.scrollX + left - 10}px`;
 });
